06f1fda80dea7f4903ca8cfddd283840ebada300
Brenton M. Wiernik <bwiernik@users.noreply.github.com>
2022-01-10 20:15:25 -0500

    Big update -- switch to DS4P structure

--- data/daily_consumption.R
+++ data/daily_consumption.R
@@ -2,14 +2,14 @@ library(tidyverse)
 library(lubridate)
 library(here)
 ## Data from http://archive.ics.uci.edu/ml/datasets/Individual+household+electric+power+consumption#
-dat <- here("data", "household_power_consumption.txt") %>% 
+dat <- here("data", "household_power_consumption.txt") |> 
   read_delim(delim = ";")
 
-dat %>% 
-    group_by(Date) %>% 
+dat |> 
+    group_by(Date) |> 
     summarise(intensity = max(Global_intensity),
-              voltage = max(Voltage)) %>% 
-    rename(date = Date) %>% 
-    mutate(date = ymd(dmy(date))) %>% 
-    na.omit() %>%     # To demonstrate implicit NA's
+              voltage = max(Voltage)) |> 
+    rename(date = Date) |> 
+    mutate(date = ymd(dmy(date))) |> 
+    na.omit() |>     # To demonstrate implicit NA's
     write_csv("daily_consumption.csv")
